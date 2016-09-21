f1a3ba1861eb0ed90c738447a2f019d3ca9f1b1d
Demis Bellot <demis.bellot@gmail.com>
2016-09-21 18:35:19 -0400

    Add simple Hello world configurations for different .NET Core templates

---
+++ src/SelfHost/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack;
+
+namespace SelfHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:55000/")
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseServiceStack(new AppHost());
+
+            app.Run(context =>
+            {
+                context.Response.Redirect("/metadata");
+                return Task.FromResult(0);
+            });
+        }
+    }
+
+
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class MyServices : Service
+    {
+        public object Any(Hello request) =>
+            new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("ServiceStack + .NET Core Self Host", typeof(MyServices).GetTypeInfo().Assembly) { }
+
+        public override void Configure(Funq.Container container)
+        {
+        }
+    }
+}
---
+++ src/WebApp/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ServiceStack;
+using System.Reflection;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseServiceStack(new AppHost());
+
+            app.Run(context =>
+            {
+                context.Response.Redirect("/metadata");
+                return Task.FromResult(0);
+            });
+        }
+    }
+
+
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class MyServices : Service
+    {
+        public object Any(Hello request) =>
+            new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("ServiceStack + .NET Core Web App", typeof(MyServices).GetTypeInfo().Assembly) { }
+
+        public override void Configure(Funq.Container container)
+        {
+        }
+    }
+}
